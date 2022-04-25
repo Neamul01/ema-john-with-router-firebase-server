95fb0bd1bd8a55ae68371e2a0c2c9d3091817409
Neamul01 <neamulhaque2002@gmail.com>
2022-04-26 01:20:07 +0600

    products find by id for using pagination

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//midleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bgcrq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('emaJohn').collection('product');
+
+        //get all product
+        app.get('/product', async (req, res) => {
+            // console.log(req.query);
+            const page = Number(req.query.page);
+            const size = Number(req.query.size);
+            const query = {};
+            const cursor = productCollection.find(query);
+            let products;
+            if (page || size) {
+                products = await cursor.skip(page * size).limit(size).toArray();
+            }
+            else {
+                products = await cursor.toArray();
+            }
+            res.send(products);
+        })
+
+        //get product count
+        app.get('/productCount', async (req, res) => {
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({ count })
+        })
+
+        //use post to get products by ids
+        app.post('/productsByKeys', async (req, res) => {
+            const keys = req.body;
+            const ids = keys.map(key => ObjectId(key));
+            const query = { _id: { $in: ids } };
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products)
+            console.log(keys)
+
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('ema-john server is running')
+})
+
+app.listen(port, () => {
+    console.log('ema-john server running on port: ', port);
+})
